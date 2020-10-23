d762703cdf25a4b8a5e97aff8148fade1bc90ec8
Rusewww <rusewww1@gmail.com>
2020-10-23 11:35:27 -0400

    Program 4

---
+++ lab03/Program4/src/main.c
@@ -0,0 +1,12 @@
+int main()
+{
+int system=8;
+int a=1001;
+int a1=a/1000;
+int a2=a%1000/100;
+int a3=a%100/10;
+int a4=a%10;
+int aIn10=(a1*system*system*system)+(a2*system*system)+(a3*system)+a4;
+aIn10=aIn10;
+ return 0;
+}
