e9bc79535ed282e392f0c08bb0e2e538bce926b4
Rusewww <rusewww1@gmail.com>
2020-10-25 09:08:35 -0400

    Program one code style

--- lab03/Program1/src/main.c
+++ lab03/Program1/src/main.c
@@ -1,7 +1,9 @@
 int main() {
-    const float firstResistorResistance= 0.5; // Опір першого резистора
-    const float secondResistorResistance= 0.5;// Опір другого резистора
-    const float thirdResistorResistance= 0.5;// Опір трерього резистора
-    float totalResistance=(firstResistorResistance*secondResistorResistance*thirdResistorResistance)/(secondResistorResistance*thirdResistorResistance+firstResistorResistance*thirdResistorResistance+firstResistorResistance*secondResistorResistance);//Загальний опір кола та формула його обчислення
-    return 0;
+        const float res1 = 0.5 ; // Опір першого резистора
+        const float res2 = 0.5 ; // Опір другого резистора
+        const float res3 = 0.5 ; // Опір трерього резистора
+        float part1 = res1 * res2 * res3 ; //Перша частина формули
+        float part2 = res2 * res3 + res1 * res3 + res1 * res2 ; //Друга частина формули
+        float totalResistance = part1 / part2 ; //Загальний опір кола
+        return 0;
 }
