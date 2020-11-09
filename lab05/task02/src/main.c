418cd74d7046a56adadc59d77b4ff8e754324a86
Rusewww <rusewww1@gmail.com>
2020-11-09 18:33:12 -0500

    Optimization

--- lab05/task02/src/main.c
+++ lab05/task02/src/main.c
@@ -5,11 +5,14 @@ int main() {
 //Перевірка на одиницю(бо одиниця не є простим числом):
         if (num == 1) {
                 primeNum = 0 ;
+        }else if (num % 2 == 0){
+                primeNum = 0 ;
         }else{
 //Перевірка на те, чи є число простим:
-                for (int i = 2; i < num; i++) {
+                for (int i = 3; i < num; i+=2) {
                         if (num % i == 0) {
-                                primeNum = 0 ; 
+                                primeNum = 0 ;
+                                break; 
                         }
                 }
         }        
--- lab05/task03/src/main.c
+++ lab05/task03/src/main.c
@@ -5,18 +5,16 @@ int main() {
         int sum2 = 0 ;
         int numb = 0 ;
         int luckyNum = 0 ; //Якщо luckyNum = 1, тоді число "щасливе".
-//Визначення сумми цифр першої половини:
-        for (int i = 100000; i !=100; i = i / 10) {
+//Визначення сумми цифр обох половин:
+        for (int i = 100000; i >=1; i /= 10) {
                 numb = num % (i * 10) ;
-                numb = numb / i ;
-                sum1 = sum1 + numb ;
+                numb /= i ;
+                if ( i > 100) {
+                        sum1 += numb ;
+                }else{ 
+                        sum2 += numb ;
                 }
-//Визначення сумми цифр другої половини:
-        for (int i = 100; i >= 1; i = i / 10) {
-                numb = num % (i * 10) ;
-                numb = numb / i ;
-                sum2 = sum2 + numb ;
-                }
+        }
 //Перевірка на те, чи є число простим:
         if (sum1 == sum2) {
                 luckyNum = 1 ;
