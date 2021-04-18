f7d3b12354cc11569690956594eedcf629ae2e05
Rusewww <rusewww1@gmail.com>
2021-04-18 21:54:44 +0300

    Static library edit

--- lab21/libstatic/src/list.c
+++ lab21/libstatic/src/list.c
@@ -4,28 +4,6 @@
 
 #include "list.h"
 
-void menuPrint(){
-	printf("\t|=============<Main_menu>=============|\n");
-	printf("\t|1 - Show list                        |\n");
-	printf("\t|2 - Write list to file               |\n");
-	printf("\t|3 - Find with criterion              |\n");
-	printf("\t|4 - Sort by price                    |\n");
-	printf("\t|5 - Add link                         |\n");
-	printf("\t|6 - Delete link                      |\n");
-	printf("\t|7 - Exit                             |\n");
-	printf("\t|=====================================|\n");
-}
-
-void searchPrint(){
-	printf("\t|=============<Searching>=============|\n");
-	printf("\t|1 - Waterproof                       |\n");
-	printf("\t|2 - Price                            |\n");
-	printf("\t|3 - Firm                             |\n");
-	printf("\t|4 - Country                          |\n");
-	printf("\t|5 - Style                            |\n");
-	printf("\t|=====================================|\n");
-}
-
 struct list *newList()
 {
 	struct list *listOne = malloc(sizeof(struct list));
