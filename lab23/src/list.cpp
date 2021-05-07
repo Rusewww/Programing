46b2444934330a3218a1b2f6ff9a98b3a4dead73
Rusewww <Rusewww1@gmail.com>
2021-05-08 00:40:41 +0300

    Lab23: fix

--- lab23/src/list.cpp
+++ lab23/src/list.cpp
@@ -7,7 +7,7 @@ void list::setCount(const int countCopy)
 
 void list::addLink(watch watchLink)
 {
-	auto **tmp = new watch *[this->count + 2];
+	auto **tmp = new watch *[this->count + 3];
 	for (int i = 0; i < this->count; i++) {
 		tmp[i] = this->watches[i];
 	}
