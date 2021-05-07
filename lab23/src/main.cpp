4039209c8ee111a16a83a433d803a7284cb9ccb8
Rusewww <Rusewww1@gmail.com>
2021-05-07 20:54:48 +0300

    Lab23: fix

--- lab23/src/main.cpp
+++ lab23/src/main.cpp
@@ -17,8 +17,8 @@ int main()
 	watch watchTree(true, "MTG-B1000", 1000, manufacturerStruct("G-Shock", "Japan"), SPORT);
 	list *watchList = new list;
 	watchList->setCount(0);
-	watchList->addLink(watchTwo);
 	watchList->addLink(*watchOne);
+	watchList->addLink(watchTwo);
 	watchList->addLink(watchTree);
 
 	cout << "|===============<Origin_list>===============|" << endl;
