290f0ad05ee415a6270f6a76ebe8f3b7767b7c45
Rusewww <Rusewww1@gmail.com>
2021-05-07 15:23:20 +0300

    Lab23: main

--- lab23/src/main.cpp
+++ lab23/src/main.cpp
@@ -1,3 +1,24 @@
-int main(){
+#include "list.h"
+
+int main()
+{
+	watch watchOne(true, "RIM", 2200, manufacturerStruct("Mazzucato", "Italy"), ARMOURED);
+	watch watchTwo(false, "ZO9507", 1995, manufacturerStruct("Zodiac", "Germany"), CLASSIC);
+	watch watchTree(true, "MTG-B1000", 1000, manufacturerStruct("G-Shock", "Japan"), SPORT);
+	list *watchList = new list;
+	watchList->addLink(watchOne);
+	watchList->addLink(watchTwo);
+	watchList->addLink(watchTree);
+
+	watchList->showAll();
+	cout << "_______________________________________________" << endl;
+	watchList->findByPrice(1500);
+	cout << "_______________________________________________" << endl;
+	watch tmp = watchList->getLink(1);
+	tmp.show();
+	cout << "_______________________________________________" << endl;
+	watchList->removeLink(1);
+	watchList->showAll();
+	delete watchList;
 	return 0;
 }
