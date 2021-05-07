0cd0c264f2cf449887bb4dae7d50216609440b27
Rusewww <Rusewww1@gmail.com>
2021-05-07 23:13:17 +0300

    Lab23: fix

--- lab23/src/main.cpp
+++ lab23/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	/*auto *manufacturer = new manufacturerStruct;
+	auto *manufacturer = new manufacturerStruct;
 	manufacturer->setFirm("Mazzucato");
 	manufacturer->setCountry("Italy");
 
@@ -11,32 +11,32 @@ int main()
 	watchOne->setModel("RIM");
 	watchOne->setCost(2200);
 	watchOne->setManufacturer(manufacturer);
-	watchOne->setStyle(ARMOURED);*/
+	watchOne->setStyle(ARMOURED);
 
-	//watch watchTwo(false, "ZO9507", 1995, manufacturerStruct("Zodiac", "Germany"), CLASSIC);
+	watch watchTwo(false, "ZO9507", 1995, manufacturerStruct("Zodiac", "Germany"), CLASSIC);
 	watch watchTree(true, "MTG-B1000", 1000, manufacturerStruct("G-Shock", "Japan"), SPORT);
 	list *watchList = new list;
 	watchList->setCount(0);
-	//watchList->addLink(*watchOne);
-	//watchList->addLink(watchTwo);
+	watchList->addLink(*watchOne);
+	watchList->addLink(watchTwo);
 	watchList->addLink(watchTree);
 
 	cout << "|===============<Origin_list>===============|" << endl;
 	watchList->showAll();
 	cout << "|-------------------------------------------|" << endl;
 	cout << "|=======<Watch_with_price_under_1500>=======|" << endl;
-	//watchList->findByPrice(1500);
+	watchList->findByPrice(1500);
 	cout << "|-------------------------------------------|" << endl;
 	cout << "|=========<Watch_on_first_position>=========|" << endl;
-	//watch tmp = watchList->getLink(0);
-	//tmp.show();
+	watch tmp = watchList->getLink(0);
+	tmp.show();
 	cout << "|-------------------------------------------|" << endl;
 	cout << "|=======<List_with_deleted_first_link>======|" << endl;
-	//watchList->removeLink(0);
-	//watchList->showAll();
+	watchList->removeLink(0);
+	watchList->showAll();
 	cout << "|-------------------------------------------|" << endl;
 	delete watchList;
-	//delete manufacturer;
-	//delete watchOne;
+	delete manufacturer;
+	delete watchOne;
 	return 0;
 }
