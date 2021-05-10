ef66836463e707de8eefba1ac1f34449bd2079ab
Rusewww <Rusewww1@gmail.com>
2021-05-11 01:54:57 +0300

    Lab23: fix

--- lab24/src/list.cpp
+++ lab24/src/list.cpp
@@ -76,85 +76,6 @@ list::~list()
 	delete[] watches;
 }
 
-/*watch &list::ToClass(const string &sWatch)
-{
-	string clone = sWatch;
-	unsigned long length = sWatch.length();
-	string str;
-	stringstream buffer;
-	int counter = 0;
-	unsigned long position = 0;
-	auto *tmp = new watch;
-	for (unsigned long i = 0; i < length; i++) {
-		if (sWatch[i] == ' ') {
-			if (counter == 0) {
-				position = clone.find(' ');
-				str = clone.substr(0, position);
-				int waterproof;
-				buffer << str;
-				buffer >> waterproof;
-				tmp->setWaterproof(waterproof);
-				clone.erase(0, position + 1);
-				buffer.clear();
-				position = i;
-				counter++;
-			} else if (counter == 1) {
-				position = clone.find(' ');
-				str = clone.substr(0, position);
-				tmp->setModel(str);
-				clone.erase(0, position + 1);
-				counter++;
-			} else if (counter == 2) {
-				position = clone.find(' ');
-				str = clone.substr(0, position);
-				int cost;
-				buffer << str;
-				buffer >> cost;
-				tmp->setCost(cost);
-				clone.erase(0, position + 1);
-				buffer.clear();
-				counter++;
-			} else if (counter == 3) {
-				for (unsigned long j = 0; j < clone.length(); ++j) {
-					if (clone[j] == ' ') {
-						position = clone.find(' ');
-						string firm = clone.substr(0, position);
-						clone.erase(0, position + 1);
-						string country = clone.substr(0, position + 1);
-						auto *manufacturer = new manufacturerStruct(firm, country);
-						tmp->setManufacturer(manufacturer);
-						delete manufacturer;
-						break;
-					}
-				}
-				buffer.clear();
-				clone.erase(0, position + 1);
-				counter++;
-			} else if (counter == 4) {
-				position = clone.find(' ');
-				str = clone.substr(0, position);
-				int style;
-				buffer << str;
-				buffer >> style;
-				switch (style) {
-				case 0:
-					tmp->setStyle(ARMOURED);
-					break;
-				case 1:
-					tmp->setStyle(CLASSIC);
-					break;
-				case 2:
-					tmp->setStyle(SPORT);
-				default:
-					break;
-				}
-				break;
-			}
-		}
-	}
-	return *tmp;
-}*/
-
 string &list::readFromFile(const string &sList, string classL[3])
 {
 	ifstream fileInf;
@@ -162,8 +83,6 @@ string &list::readFromFile(const string &sList, string classL[3])
 	int length = 3;
 	for (int i = 0; i < length; i++) {
 		getline(fileInf, classL[i]);
-		//getline(fileInf, classL[1]);
-		//getline(fileInf, classL[2]);
 	}
 	fileInf.close();
 	return *classL;
