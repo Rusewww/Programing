38a813adbeb6a25f575389ffd851fdd350b13dd0
Rusewww <Rusewww1@gmail.com>
2021-05-20 17:32:34 +0300

    Lab25: edit

--- lab25/src/list.cpp
+++ lab25/src/list.cpp
@@ -162,7 +162,7 @@ ifstream &operator>>(ifstream &in, list &list1)
 {
 	delete[] list1.watches;
 	list1.watches = new watch *[list1.count + 1];
-	for (size_t i = 0; i < (size_t)list1.count; ++i) {
+	for (size_t i = 0; i <= (size_t)list1.count; ++i) {
 		list1.watches[i] = new watch;
 	}
 	regex reg("^[0-1] [A-Z][a-z]* [0-9]{1,6} [0-2] [a-zA-Z.&]* [A-Z][a-zA-Z]*");
