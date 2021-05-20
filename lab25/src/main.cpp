f5c4957a5a2c011f42a202dfb3b9a5f48cc021aa
Rusewww <Rusewww1@gmail.com>
2021-05-20 17:26:57 +0300

    Lab25: edit

--- lab25/src/main.cpp
+++ lab25/src/main.cpp
@@ -9,13 +9,12 @@ int main()
 	cout << "|-------------------------------------------|" << endl;
 	cout << "|==============<Overloaded '='>=============|" << endl;
 	watch watchOne(true, "ZO9507", 1995, manufacturerStruct("Zodiac", "Germany"), CLASSIC);
-	auto *watchTree = new watch;
-	watchTree = &watchOne;
-	cout << *watchTree;
+	watch watchTree;
+	watchTree = watchOne;
+	cout << watchTree;
 	cout << "|-------------------------------------------|" << endl;
 
 	watchList->writeToFile("../dist/output.txt");
 	delete watchList;
-	delete watchTree;
 	return 0;
 }
