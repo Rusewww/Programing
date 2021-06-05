eb16c66f4eba581d7522d5e69db209577d1259ba
Rusewww <Rusewww1@gmail.com>
2021-06-05 15:46:49 +0300

    RGZ: controller.h - doxygen documentation

--- RGZ/src/controller.h
+++ RGZ/src/controller.h
@@ -3,23 +3,66 @@
 
 #include "list.h"
 
+/**
+ * controller - клас контроллеру
+ */
+
 class controller {
 public:
     list watchList;
 
+    /**
+     * Конструктор класу controller за замовчуванням
+     */
+
     controller();
+
+    /**
+     * Деструктор класу controller
+     */
 
     ~controller();
 
+    /**
+     * setList - сеттетер класу controller, який записує данні у список watchList
+     * @param watchListCopy
+     */
+
     void setList(list *watchListCopy);
+
+    /**
+     * showList - виводить вміст списку
+     */
 
     void showList() const;
 
+    /**
+     * sorting - сортує список відповідно до вхідних параметрів
+     * @param way - напрямок сортування
+     * @param criterion - критерій сортування
+     */
+
     void sorting(char way, int criterion);
+
+    /**
+     * find - знаходить ланку відповідно до критеріїв
+     * @param criterion - критерій пошуку
+     * @return Вектор необхідної ланки
+     */
 
     vector<mechanicalWatches *> find(int criterion) const;
 
-    bool readFromFile(const string &path);
+    /**
+     * readFromFile - зчитує данні з файлу
+     * @param path - шлях до файлу з якого необхідно зчитати данні
+     */
+
+    void readFromFile(const string &path);
+
+    /**
+     * writeToFile - записує данні до файлу
+     * @param path - шлях до файлу в який необхідно записати данні
+     */
 
     void writeToFile(const string &path);
 };
