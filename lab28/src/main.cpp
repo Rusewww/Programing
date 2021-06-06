e2c1f9407fb90e7142ff05859bf155d6bc66e79f
Rusewww <Rusewww1@gmail.com>
2021-06-06 22:55:22 +0300

    lab28: Fix

--- lab28/src/main.cpp
+++ lab28/src/main.cpp
@@ -7,8 +7,9 @@ int main() {
             string fileName;
             cout << "Enter name of input file: ";
             cin >> fileName;
-            if (fileName == "\\exit")
+            if (fileName == "\\exit") {
                 return 0;
+            }
             menu.setFileName(fileName);
             cout << "Finally!" << endl;
             menu.readFromFile();
@@ -16,21 +17,22 @@ int main() {
             menu.setResult(result);
             cout << endl << "Enter name of output file: ";
             cin >> fileName;
-            if (fileName == "\\exit")
+            if (fileName == "\\exit") {
                 return 0;
+            }
             menu.setFileName(fileName);
             cout << "Finally!" << endl;
             menu.writeToFile();
             break;
         }
-        catch (out_of_range &e) {
-            cout << e.what() << endl;
+        catch (out_of_range &outR) {
+            cout << outR.what() << endl;
         }
-        catch (ios_base::failure &e) {
-            cout << e.what() << endl;
+        catch (ios_base::failure &outR) {
+            cout << outR.what() << endl;
         }
-        catch (invalid_argument &e) {
-            cout << e.what() << endl;
+        catch (invalid_argument &outR) {
+            cout << outR.what() << endl;
         }
     }
 }
